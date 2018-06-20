0ab67ef5e6733db13da6a5df5e0877fd21f80b4b
Corey <hutcheson.coreyJ@gmail.com>
2018-06-19 19:03:02 -0500

    removed height and width from custom logo functions

--- functions.php
+++ functions.php
@@ -80,10 +80,7 @@ if ( ! function_exists( 'mayhem_setup' ) ) :
 		 *
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 150,
-			'width'       => 150,
-		) );
+		add_theme_support( 'custom-logo' );
 	}
 endif;
 add_action( 'after_setup_theme', 'mayhem_setup' );
