1f9968bd36c1bc30cf82261cd59660759ea0e7ec
Corey <hutcheson.coreyJ@gmail.com>
2018-06-30 13:46:16 -0500

    created content-page-roster.php file

--- template-parts/content-page-roster.php
+++ template-parts/content-page-roster.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying gallery content
+ * Template part for displaying roster content
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -10,6 +10,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
 
 	<?php mayhem_post_thumbnail(); ?>
 
