22d562698f7585eab31e34a74e61748a7679cb54
Corey <hutcheson.coreyJ@gmail.com>
2018-06-30 14:30:47 -0500

    started loop for roster archive

--- archive-roster.php
+++ archive-roster.php
@@ -13,27 +13,21 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
+		$args = array(
+			'post_type' => 'roster'
+		);
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+		$query = new WP_Query($args);
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		if ( $query->have_posts() ) :
+			while ( $query->have_posts() ) :
+				$query->the_post(); ?>
+				<!-- Loop content goes here! -->
+				<h1>Title!</h1>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
 
+		<?php
 			endwhile;
 
 			the_posts_navigation();
