198f87915e8a225f2536f7aedbfb9622c9a708c5
Corey <hutcheson.coreyJ@gmail.com>
2018-07-13 16:38:13 -0500

    Removes .js-card-expanded class from all cards except clickedCard

--- js/roster.js
+++ js/roster.js
@@ -1,43 +1,64 @@
 (function() {
-	let choiceContainer = document.querySelector('.c-choice-toggle');
-	let mainRosterContainer = document.querySelector('.roster-container');
-	let rosterContainers = mainRosterContainer.children;
+	const choiceContainer = document.querySelector('.c-choice-toggle');
+	const mainRosterContainer = document.querySelector('.roster-container');
+	const rosterContainers = mainRosterContainer.children;
 
-	/**
-	 * Shows and Hides the appropriate roster divs when toggle button
-	 * changes
-	 */
 	choiceContainer.addEventListener('click', function(e) {
 		let target = e.target;
 
-		if (target.nodeName !== 'LABEL') return;
-
-		let value = target.getAttribute('for');
-		let neededClass = `roster-container__${value}`;
-
-		for (let container of rosterContainers) {
-			if (value === 'all') {
-				container.classList.remove('hide');
-			} else if (!container.classList.contains(neededClass)) {
-				container.classList.add('hide');
-			} else {
-				container.classList.remove('hide');
-			}
+		// Toggle Label was clicked
+		if (target.nodeName === 'LABEL') {
+			clickedChoiceToggle(target, rosterContainers);
+			return;
 		}
 	});
 
 	mainRosterContainer.addEventListener('click', function(e) {
 		let target = e.target;
 
-		// Return if not hover-element
-		if (!target.classList.contains('c-roster-card__hover-element')) {
+		// Hover Element (More/Details Btn) was clicked
+		if (target.classList.contains('c-roster-card__hover-element')) {
+			clickedHoverElement(target);
 			return;
 		}
-
-		// Gets hover-elements parent card element
-		let clickedCard = target.closest('.c-roster-card');
-		
-
 	})
+
 })();
 
+/**
+ * Shows and Hides the appropriate roster divs when toggle button
+ * changes
+ */
+function clickedChoiceToggle(target, rosterContainers) {
+	let value = target.getAttribute('for');
+	let neededClass = `roster-container__${value}`;
+
+	for (let container of rosterContainers) {
+		if (value === 'all') {
+			container.classList.remove('hide');
+		} else if (!container.classList.contains(neededClass)) {
+			container.classList.add('hide');
+		} else {
+			container.classList.remove('hide');
+		}
+	}
+}
+
+function clickedHoverElement(target) {
+	let clickedCard = target.closest('.c-roster-card');
+
+	// Add class to card to show in a modal view
+	// Add left and right arrows that will cycle through siblings
+			// Removing class from current and adding to siblings
+	
+	// Remove js-card-expanded from all cards except clickedCard
+	let allCards = document.querySelectorAll('.c-roster-card');
+	for (let card of allCards) {
+		if (card === clickedCard) {
+			card.classList.add('js-card-expanded');
+		} else {
+			card.classList.remove('js-card-expanded');
+		}
+	}
+
+}
