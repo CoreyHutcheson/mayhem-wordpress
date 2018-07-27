e993515eeef5c7b5cd7a3d99e36b2f967c6b44be
Corey <hutcheson.coreyj@gmail.com>
2018-07-27 15:35:58 -0500

    dynamic markup for events

--- page-events.php
+++ page-events.php
@@ -11,7 +11,7 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main event-main">
+		<main id="main" class="site-main events-container">
 
 			<?php 
 			// Any flyers with event_date yet to pass (later than current
@@ -20,20 +20,26 @@ get_header();
 				'post_type' => 'flyer',
 				'meta_key' => 'event_date',
 				'meta_value'   => date( "Ymd" ),
-				'meta_compare' => '>',
+				'meta_compare' => '>=',
 			);
 
 			$query = new WP_Query($args);
 
-			if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+			if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
 
-				<?php if (get_field('featured_flyer')) : ?>
-					<p>Featured</p>
-					<div><?php mayhem_post_thumbnail(); ?></div>
-				<?php else : ?>
-					<p>Not Featured</p>
-					<div><?php mayhem_post_thumbnail(); ?></div>
-				<?php endif; ?>
+				$featured = get_field('featured_flyer') ?>
+
+				<div class="event">
+					<div class="event__flyer <?php if ($featured) {echo 'event__flyer--featured';} ?>">
+						<?php if($featured) : ?>
+							<div class="featured-banner">
+								<span>Featured!!!</span>
+							</div>
+						<?php endif; ?>
+
+						<?php mayhem_post_thumbnail(); ?>
+					</div>
+				</div>
 
 			<?php endwhile; endif;
 
