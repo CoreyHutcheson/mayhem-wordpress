1aa9fb0a0b1f36962088ade9d86e9a326292137b
Corey <hutcheson.coreyj@gmail.com>
2018-07-27 12:30:45 -0500

    commented mayhem_modify_post function

--- inc/custom-hooks/flyer-hooks.php
+++ inc/custom-hooks/flyer-hooks.php
@@ -5,17 +5,17 @@
  * @param  [array] $data : the slashed post data
  * @return [array] : modified post data
  */
-function mayhem_modify_post_title($data) {
-
+function mayhem_modify_post($data) {
 	/* Changes flyer post type title and slug
 	 * Title Format = October 27, 2018
 	 * Slug/Name Format = October-27-2018 
 	 */
   if ($data['post_type'] === 'flyer' && get_field('event_date')) {
-  	$nameDate = date('F-j-Y', strtotime(get_field('event_date')));
-  	$data['post_title'] = get_field('event_date');
+    $date = get_field('event_date');
+  	$nameDate = date('F-j-Y', strtotime($date));
+  	$data['post_title'] = $date;
   	$data['post_name'] = $nameDate;
   }
   return $data; // Returns the modified data.
 }
-add_filter('wp_insert_post_data', 'mayhem_modify_post_title', '10', 1);
+add_filter('wp_insert_post_data', 'mayhem_modify_post', '10', 1);
