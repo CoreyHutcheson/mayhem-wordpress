ef0c0dab93cf143498182e7f6607acc257b784b7
Corey <hutcheson.coreyJ@gmail.com>
2018-07-30 10:34:16 -0500

    Added event date and borders to events and event-notifications

--- page-events.php
+++ page-events.php
@@ -10,53 +10,66 @@
 get_header();
 ?>
 
-<?php 
-$args = array(
-	'post_type' => 'flyer',
-	'order' => 'ASC',
-	'orderby' => 'meta_value_num',
-	'meta_key' => 'event_date',
-	'meta_query' => array(
-		array(
-			'key' => 'featured_flyer',
-			'value' => true,
+<?php // Query to create featured-notification divs
+	$args = array(
+		'post_type' => 'flyer',
+		'order' => 'ASC',
+		'orderby' => 'meta_value_num',
+		'meta_key' => 'event_date',
+		'meta_query' => array(
+			array(
+				'key' => 'featured_flyer',
+				'value' => true,
+			),
+			array(
+				'key' => 'event_date',
+				'value' => date( 'Ymd' ),
+				'compare' => '>=',
+			),
 		),
-		array(
-			'key' => 'event_date',
-			'value' => date( 'Ymd' ),
-			'compare' => '>=',
-		),
-	),
-);
+	);
 
-$query = new WP_Query($args);
+	$query = new WP_Query($args);
 
-if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+	if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
-	<div class="featured-notification">
-		<i class="fas fa-star"></i>
-		<span>Featured:</span>
-		<span>
-			<a href="<?php the_permalink(); ?>" class="featured-notification__link"><?php the_title(); ?></a>
-		</span>
-		<i class="fas fa-star"></i>
-	</div>
+		<div class="featured-notification">
+			<i class="fas fa-star"></i>
+			<span>Featured:</span>
+			<span>
+				<a href="<?php the_permalink(); ?>" class="featured-notification__link"><?php the_title(); ?></a>
+			</span>
+			<i class="fas fa-star"></i>
+		</div>
 
-<?php endwhile; endif;
-wp_reset_postdata();
+	<?php endwhile; endif;
+	wp_reset_postdata();
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main events-container">
 
 			<?php 
-			// Any flyers with event_date yet to pass (later than current
-			// date/time)
+			// Any flyers with event_date that hasn't passed yet
+			// Featured Flyers first, then non-featured
+			// Events that are a later date are lower down.
 			$args = array(
 				'post_type' => 'flyer',
-				'meta_key' => 'event_date',
-				'meta_value'   => date( "Ymd" ),
-				'meta_compare' => '>=',
+				'meta_query' => array(
+					'date' => array(
+						'key' => 'event_date',
+						'value' => date("Ymd"),
+						'compare' => '>=',
+					),
+					'featured' => array(
+						'key' => 'featured_flyer',
+						'compare' => 'EXISTS',
+					),
+				),
+				'orderby' => array(
+					'featured' => 'DESC',
+					'date' => 'ASC',
+				),
 			);
 
 			$query = new WP_Query($args);
@@ -66,6 +79,7 @@ wp_reset_postdata();
 				$featured = get_field('featured_flyer') ?>
 
 				<div class="event">
+					<div class="event__date"><?php the_title(); ?></div>
 					<div class="event__flyer <?php if ($featured) {echo 'event__flyer--featured';} ?>">
 						<?php if($featured) : ?>
 							<div class="featured-banner">
@@ -73,7 +87,7 @@ wp_reset_postdata();
 							</div>
 						<?php endif; ?>
 
-						<?php mayhem_post_thumbnail(); ?>
+						<a href="<?php the_permalink(); ?>" class="event__link"><?php mayhem_post_thumbnail(); ?></a>
 					</div>
 				</div>
 
