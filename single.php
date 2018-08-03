03e5afc801911d7c254e09bbf622249aea7849b0
Corey <hutcheson.coreyj@gmail.com>
2018-08-03 14:29:25 -0500

    Began work on custom pagination

--- single.php
+++ single.php
@@ -19,7 +19,8 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			// Custom Pagination
+			getPrevNext();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -35,3 +36,44 @@ get_header();
 <?php
 get_sidebar();
 get_footer();
+
+function getPrevNext(){
+	$eventList = get_posts(array(
+		'post_type' => 'flyer',
+		'numberposts' => -1,
+	));
+
+	$events = array();
+
+	foreach ($eventList as $event) {
+	   $events[] += $event->ID;
+	}
+
+	$current = array_search(get_the_ID(), $events);
+	$prevID = $events[$current - 1];
+	$nextID = $events[$current + 1];
+	
+	echo '<div class="navigation">';
+	
+	if (!empty($prevID)) {
+		echo '<div class="alignleft">';
+		echo '<a href="';
+		echo get_permalink($prevID);
+		echo '"';
+		echo 'title="';
+		echo get_the_title($prevID); 
+		echo'">Previous</a>';
+		echo "</div>";
+	}
+	if (!empty($nextID)) {
+		echo '<div class="alignright">';
+		echo '<a href="';
+		echo get_permalink($nextID);
+		echo '"';
+		echo 'title="';
+		echo get_the_title($nextID); 
+		echo'">Next</a>';
+		echo "</div>";		
+	}
+}	
+?>
--- template-parts/content-flyer.php
+++ template-parts/content-flyer.php
@@ -28,10 +28,10 @@ $articleClasses = get_the_content() ?
 		<?php
 		the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mayhem' ),
-			'after'  => '</div>',
-		) );
+		// wp_link_pages( array(
+		// 	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mayhem' ),
+		// 	'after'  => '</div>',
+		// ) );
 		?>
 	</div><!-- .entry-content -->
 
