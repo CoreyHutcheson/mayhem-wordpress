d420fc06c89312a14973799aee8a42c8cc0ce490
Corey <hutcheson.coreyj@gmail.com>
2018-08-07 20:46:09 -0500

    Moved custom flyer pagination / Deleted single-flyer.php / Added conditional to single.php for custom pagination

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -271,3 +271,62 @@ if ( ! function_exists( 'wp_get_attachment_by_file_name' ) ) :
           return false;
 	}
 endif;
+
+/**
+ * Custom pagination by event date for Flyer/Event single page
+ */
+if ( ! function_exists('mayhem_custom_flyer_pagination')) : 
+
+	function mayhem_custom_flyer_pagination() {
+		// Gets all flyer posts that haven't occurred yet
+		// ordering by event_date (next event first)
+		$eventList = get_posts(array(
+			'post_type' => 'flyer',
+			'numberposts' => -1,
+			'meta_query' => array(
+				'date' => array(
+					'key' => 'event_date',
+					'value' => date("Ymd"),
+					'compare' => '>=',
+				),
+			),
+			'orderby' => array(
+				'date' => 'ASC',
+			),
+		));
+
+		// Creates array of sorted event IDs (by date of event)
+		$orderedIDs = array_map(function($event) {
+			return $event->ID;
+		}, $eventList);
+
+		// Returns index of current event in $eventList
+		$current = array_search(get_the_ID(), $orderedIDs);
+
+		// Sets Prev and Next Event IDs
+		$prevID = ($current > 0) ? $eventList[$current - 1] : NULL;
+		$nextID = ($current < count($orderedIDs) - 1) ? $eventList[$current + 1] : NULL;
+		?>
+
+		<div class="navigation">
+
+			<?php if (!empty($prevID)) : ?>
+				<div class="alignleft">
+					<a href="<?php the_permalink($prevID); ?>" title="<?php echo get_the_title($prevID); ?>">
+						Previous Event
+					</a>
+				</div>
+			<?php endif; ?>
+
+			<?php if (!empty($nextID)) : ?>
+				<div class="alignright">
+					<a href="<?php the_permalink($nextID); ?>" title="<?php echo get_the_title($nextID); ?>">
+						Next Event
+					</a>
+				</div>
+			<?php endif; ?>
+
+		</div> <!-- /.navigation -->
+		<?php
+	}
+endif;
--- single.php
+++ single.php
@@ -19,7 +19,12 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			if (is_singular('flyer')) :
+				// Located in inc/template-tags.php
+				mayhem_custom_flyer_pagination();
+			else : 
+				the_post_navigation();
+			endif;
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
