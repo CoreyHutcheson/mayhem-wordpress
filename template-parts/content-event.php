e11db2c920bc8bd73c75e13dcb4d84321138d8e1
Corey <hutcheson.coreyJ@gmail.com>
2018-09-10 10:09:45 -0500

    Added the_content to output mt-order form / changed flyer-entry to event on both content-events and corresponding sass files

--- template-parts/content-event.php
+++ template-parts/content-event.php
@@ -12,20 +12,20 @@
 <?php 
 // Conditionally get the classes to add to the article used for positioning
 $articleClasses = get_field('card_details') ? 
-	array('flyer-entry', 'flyer-entry--content') : 'flyer-entry';
+	array('event', 'event--content') : 'event';
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class($articleClasses); ?>>
 
-	<header class="flyer-entry__header">
-		<?php the_title( '<h1 class="flyer-entry__title">', '</h1>' ); ?>
+	<header class="event__header">
+		<?php the_title( '<h1 class="event__title">', '</h1>' ); ?>
 	</header>
 
-	<div class="post-thumbnail flyer-entry__thumbnail">
+	<div class="post-thumbnail event__thumbnail">
 		<?php echo wp_get_attachment_image( get_field('event_image'), 'full' ); ?>
 	</div>
 
-	<div class="flyer-entry__content">
+	<div class="event__content">
 
 		<?php // Output all match descriptions and matches
 		if (have_rows('card_details')) :
@@ -37,12 +37,12 @@ $articleClasses = get_field('card_details') ?
 
 				if ($description) :
 					$match_details .= 
-						"<div class='flyer-entry__match-description'>
+						"<div class='event__match-description'>
 							${description}
 						</div>";
 				endif;
 
-				$match_details .= "<div class='flyer-entry__match'>${match}</div>";
+				$match_details .= "<div class='event__match'>${match}</div>";
 			
 				echo $match_details;
 			endwhile;
@@ -51,17 +51,31 @@ $articleClasses = get_field('card_details') ?
 		// Outputs extra details if available
 		if (get_field('extra_card_details')) :
 			$extra = 
-				"<div class='flyer-entry__extra-card-details'>"
+				"<div class='event__extra-card-details'>"
 				 . get_field('extra_card_details') .
 				"</div>";
 			echo $extra;
 		endif;
+
+		// Outputs the my-tickets order form if applicable
+		the_content( sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'mayhem' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		) );
 		?>
 
 	</div>
 
 	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="flyer-entry__footer">
+		<footer class="event__footer">
 			<?php
 			edit_post_link(
 				sprintf(
