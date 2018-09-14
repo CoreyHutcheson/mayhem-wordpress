2a63c2760b52670b762a099248a3d3feaa9fe8bb
Corey <hutcheson.coreyJ@gmail.com>
2018-09-14 14:19:13 -0500

    working bulk-tickets without errors

--- bulk-tickets.php
+++ bulk-tickets.php
@@ -12,107 +12,100 @@
 ?>
 <html>
 <head>
-	<title><?php bloginfo( 'blogname' ); ?> &bull; <?php _e( 'Tickets', 'my-tickets' ); ?> &bull; <?php mt_ticket_id(); ?></title>
+	<title><?php bloginfo( 'blogname' ); ?> &bull; <?php _e( 'Tickets', 'my-tickets' ); ?> &bull; <?php mt_receipt_id(); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link type="text/css" rel="stylesheet" href="<?php echo plugins_url('my-tickets/templates/css/generic.css'); ?>"/>
 	<link type="text/css" rel="stylesheet" href="<?php echo plugins_url('my-tickets/templates/css/ticket.css'); ?>"/>
 </head>
 
 <body class="bulk-tickets">
-<?php
+<?php 
 	// load data from the Receipts Page.
 	if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 			// load data from the Tickets Page.
 			$purchases = mt_get_payment_tickets();
 
-			foreach ( $purchases as $ticket_id ) {
-				?>
-				<div class='panel ticket <?php mt_ticket_method( $ticket_id ); ?>'>
+			foreach ( $purchases as $ticket_id ) : 
+				$ticket_method = mt_get_ticket_method($ticket_id); ?>
+
+				<div class='panel ticket <?php echo $ticket_method ?>'>
+
 					<div class='inside'>
-						<?php
-						if ( 'eticket' != mt_get_ticket_method( $ticket_id ) ) {
-							?>
+
+						<?php if ( 'eticket' != $ticket_method ) : ?>
 							<div class='post-thumbnail'>
-								<?php mt_logo( array(), get_the_ID() ); ?>
+								<?php mayhem_the_ticket_logo($ticket_id); ?>
 							</div>
-							<?php
-						} else {
-							?>
+						<?php else : ?>
 							<div class='ticket-qrcode'>
 								<img src="<?php mt_ticket_qrcode( $ticket_id ); ?>" alt="<?php __( 'QR Code Verification Link', 'my-tickets' ); ?>"/>
 							</div>
-							<?php
-						}
-						?>
+						<?php endif; ?>
+
 						<div class="ticket-data">
 							<h1 class='event-title'>
 								<?php mt_event_title( $ticket_id ); ?>
 							</h1>
 
 							<div class='event-date'>
-								<?php mt_event_date( $ticket_id ); ?> @ <span
-										class='time'><?php mt_event_time( $ticket_id ); ?></span>
+								<?php mt_event_date( $ticket_id ); ?> @ 
+								<span class='time'><?php mt_event_time( $ticket_id ); ?></span>
 							</div>
+
 							<div class='ticket-type'>
 								<?php mt_ticket_type( $ticket_id ); ?>
 							</div>
+
 							<div class='ticket-price'>
 								<?php mt_ticket_price( $ticket_id ); ?>
 							</div>
+
 							<div class='ticket-venue'>
 								<?php mt_ticket_venue( $ticket_id ); ?>
 							</div>
-							<?php
-							if ( 'eticket' != mt_get_ticket_method( $ticket_id ) ) {
-								?>
+
+							<?php if ( 'eticket' != $ticket_method ) : ?>
 								<div class='ticket-qrcode'>
 									<img src="<?php mt_ticket_qrcode( $ticket_id ); ?>" alt="QR Code Verification Link"/>
 								</div>
-								<?php
-							}
-							?>
+							<?php endif; ?>
+
 							<div class='post-content'>
-								<?php
+								<?php 
 								$content = get_the_content();
-								if ( '' == trim( strip_tags( $content ) ) ) {
+
+								if ( '' == trim( strip_tags( $content ) ) ) :
 									$content = ( current_user_can( 'edit_pages' ) ) ? __( 'Add your custom text into the post content.', 'my-tickets' ) : '';
-								}
+								endif;
+
 								echo $content;
 								?>
 								<?php edit_post_link(); ?>
 							</div>
-							<?php
-							if ( 'eticket' == mt_get_ticket_method( $ticket_id ) ) {
-								?>
+
+							<?php if ( 'eticket' == $ticket_method ) : ?>
 								<div class='post-thumbnail'>
-									<?php
-									if ( has_post_thumbnail() ) {
-										the_post_thumbnail();
-									} else {
-										mt_logo();
-									}
-									?>
+									<?php mayhem_the_ticket_logo($ticket_id); ?>
 								</div>
-								<?php
-							}
-							?>
+							<?php endif; ?>
+
 							<div class='ticket_id'>
 								<?php echo $ticket_id; ?>
 							</div>
-							<?php echo apply_filters( 'mt_custom_ticket', '', $ticket_id, mt_get_ticket_method() ); ?>
+
+							<?php /*echo apply_filters( 'mt_custom_ticket', '', $ticket_id, mt_get_ticket_method() ); */?>
+
 						</div>
 
 					</div>
 				</div>
-				<?php
-			}
+			<?php endforeach; ?>
 
-			if ( 'printable' == mt_get_ticket_method( $ticket_id ) ) {
-				?>
+			
+			<?php if ( 'printable' == mt_get_ticket_method( $ticket_id ) ) : ?>
 				<a href="javascript:window.print()" class="print">Print</a>
-				<?php
-			}
+			<?php endif;
 
 	endwhile; endif;
 	?>
@@ -120,11 +113,11 @@
 </html>
 
 <?php
-function mayhem_ticket_logo() {
-	$page = get_page_by_title(mt_get_event_title(), OBJECT, 'event');
+function mayhem_get_ticket_logo($ticket_id) {
+	$page = get_page_by_title(mt_get_event_title($ticket_id), OBJECT, 'event');
 	$use_custom_images = get_field('use_custom_images', $page->ID);
 	$tickets = get_field('tickets', $page->ID);
-	$ticket_type = mt_get_ticket_type();
+	$ticket_type = mt_get_ticket_type($ticket_id);
 	$size = 'thumbnail';
 	$image = '';
 
@@ -143,5 +136,9 @@ function mayhem_ticket_logo() {
 		return mt_logo( array(), get_the_ID() );
 	}
 
-	echo wp_get_attachment_image( $image, $size );
+	return wp_get_attachment_image( $image, $size );
+}
+
+function mayhem_the_ticket_logo($ticket_id) {
+	echo mayhem_get_ticket_logo($ticket_id);
 }
