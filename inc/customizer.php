7bc322e6a25cfe9f714195328fb44e56702e835d
Corey <hutcheson.coreyJ@gmail.com>
2018-09-26 14:15:32 -0500

    working customizer brand_color

--- inc/customizer.php
+++ inc/customizer.php
@@ -13,43 +13,56 @@
 function mayhem_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial( 'blogname', array(
-			'selector'        => '.site-title a',
-			'render_callback' => 'mayhem_customize_partial_blogname',
-		) );
-		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
-			'selector'        => '.site-description',
-			'render_callback' => 'mayhem_customize_partial_blogdescription',
-		) );
-	}
+	mayhem_create_custom_options($wp_customize);
 }
 add_action( 'customize_register', 'mayhem_customize_register' );
-
-/**
- * Render the site title for the selective refresh partial.
- *
- * @return void
- */
-function mayhem_customize_partial_blogname() {
-	bloginfo( 'name' );
-}
-
-/**
- * Render the site tagline for the selective refresh partial.
- *
- * @return void
- */
-function mayhem_customize_partial_blogdescription() {
-	bloginfo( 'description' );
-}
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function mayhem_customize_preview_js() {
-	wp_enqueue_script( 'mayhem-customizer', get_template_directory_uri() . '/src/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'mayhem-customizer', get_template_directory_uri() . '/src/js/customizer.js', array( 'customize-preview' ), microtime(), true );
 }
 add_action( 'customize_preview_init', 'mayhem_customize_preview_js' );
+
+/**
+ * Injects customizer css into the head tag
+ * @return [undefined]
+ */
+function mayhem_customize_css() {
+	?>
+		<style type="text/css">
+			.menu-toggle-btn span {
+				background: <?php echo get_theme_mod('brand_color', '#f8fa2c'); ?>;
+			}
+		</style>
+	<?php
+}
+add_action( 'wp_head', 'mayhem_customize_css' );
+
+/**
+ * Creates custom settings and controls
+ * @param  [object] $wp_customize
+ * @return [undefined]
+ */
+function mayhem_create_custom_options($wp_customize) {
+	/** Brand Color */
+	$wp_customize->add_setting( 'brand_color',
+	  array(
+	  	'default' => '#f8fa2c',
+	    'transport' => 'postMessage',
+	    'sanitize_callback' => 'sanitize_hex_color'
+	  )
+	);
+	$wp_customize->add_control( 'brand_color',
+		array(
+			'label' => __( 'Brand Color' ),
+      'description' => esc_html__( "The site's primary color" ),
+      'section' => 'colors',
+      'type' => 'color'
+    )
+	);
+
+	/** ... */
+}
--- src/js/customizer.js
+++ src/js/customizer.js
@@ -5,37 +5,27 @@
  *
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
-
-export default function customizer($) {
-  // Site title and description.
-  wp.customize('blogname', (value) => {
-    value.bind((to) => {
-      $('.site-title a').text(to);
-    });
-  });
-  wp.customize('blogdescription', (value) => {
-    value.bind((to) => {
-      $('.site-description').text(to);
+jQuery(document).ready(function($) {
+  /** Site Title */
+  wp.customize('blogname', control => {
+    control.bind(value => {
+      $('.site-title a').text(value);
     });
   });
 
-  // Header text color.
-  wp.customize('header_textcolor', (value) => {
-    value.bind((to) => {
-      if (to === 'blank') {
-        $('.site-title, .site-description').css({
-          clip: 'rect(1px, 1px, 1px, 1px)',
-          position: 'absolute',
-        });
-      } else {
-        $('.site-title, .site-description').css({
-          clip: 'auto',
-          position: 'relative',
-        });
-        $('.site-title a, .site-description').css({
-          color: to,
-        });
-      }
+  /** Site Description */
+  wp.customize('blogdescription', control => {
+    control.bind(value => {
+      $('.site-description').text(value);
     });
   });
-}
+
+  /** Brand Color */
+  wp.customize('brand_color', control => {
+    control.bind(value => {
+      $('.menu-toggle-btn span').css('background', value);
+    });
+  });
+
+
+});
